fc40baa68525a8e8de3fc0de8f275e1f2807f5cc
Alan Barahona Ruiz <barahonaruiz.alan@gmail.com>
2020-12-14 16:35:19 -0300

    Added main cookie functioanality

--- routes/index.js
+++ routes/index.js
@@ -7,18 +7,44 @@ router.get('/', cors(), (req, res) => {
     res.send('Hello from HTTPS');
 });
 
-router.get('/set', cors(options), (req, res) => {
-    res.writeHead(200, {
-        'Set-Cookie': 'name=key_value; secure;domain=testing.com'
-    }).send();
+router.post('/login', cors(options), (req, res) => {
+    if (req.cookies.name) {
+        return res.status(400).send();
+    }
+    res.cookie('name', 'value', {
+        secure: true,
+        domain: 'testing.com',
+        maxAge: 3600000
+    }).status(200).send();
 });
 
-router.get('/get', cors(options), (req, res) => {
+router.get('/authorize', cors(options), (req, res) => {
     if (!req.cookies.name) {
         return res.status(401).send();
     }
-    console.log(req.cookies);
-    res.status(200).send();
+    res.status(200).send('Por implementar');
 })
+
+router.get('/verify', cors(options), (req, res) => {
+    console.log(req.cookies);
+    if (!req.cookies.name) {
+        return res.status(400).send();
+    }
+    res.status(200).send(req.cookies.name);
+});
+
+router.options('/logout', cors(options), (req, res, next) => {
+    next();
+})
+router.delete('/logout', cors(options), (req, res) => {
+    console.log(req.cookies);
+    if (!req.cookies.name) {
+        return res.status(400).send();
+    }
+    res.clearCookie('name', {
+        domain: 'testing.com',
+        path: '/'
+    }).status(200).send();
+});
 
 module.exports = router;
